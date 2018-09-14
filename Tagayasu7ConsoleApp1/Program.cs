3e186da5851aedb4f2f31988521ba8f907007518
Kuni <kuni.ujiie@grapecity.com>
2018-09-14 17:06:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Tagayasu7ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tagayasu7ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Console.WriteLine("タガヤスのイベント履歴を表示");
+            Console.WriteLine("\r\n");
+
+            List<Event.EventItem> result;
+            result = Event.GetEvent("タガヤス");
+
+            foreach (Event.EventItem a in result)
+            {
+                Console.WriteLine(a.title);
+                Console.WriteLine(a.place);
+                Console.WriteLine(a.event_url);
+                Console.WriteLine("\r\n");
+                
+            }
+        }
+    }
+}
